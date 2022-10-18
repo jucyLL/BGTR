edcbc32ceb35795e53cf49a2b38cce963170861c
shen <35886465+jucyLL@users.noreply.github.com>
2022-10-18 18:16:56 +0800

    Update DTAV.py

--- DTAV Measurement/DTAV.py
+++ DTAV Measurement/DTAV.py
@@ -1,16 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 18 11:14:42 2022
-
-@author: ZLS
-"""
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jul 20 21:51:28 2022
-
-@author: ZLS
-"""
-# -*- coding: utf-8 -*-
 import os.path
 import glob
 import cv2
